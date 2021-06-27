8eb149539bf9ca4ddac2cccb96cf8ad1c2496e09
Guillermo Hiroma <guillermo.hiroma@gmail.com>
2021-06-27 18:51:25 +0900

    default port for bootstrap

--- src/configuration/index.ts
+++ src/configuration/index.ts
@@ -1,4 +1,5 @@
 export default () => ({
   region: process.env.REGION || 'ap-northeast-1',
   apiVersion: process.env.API_VERSION || '2012-08-10',
+  port: process.env.PORT || 3000
 });
--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { InternalServerErrorFilter } from './filter/internalServerError.filter';
 import { Settings } from 'luxon';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
 
   app.useGlobalFilters(new InternalServerErrorFilter());
   app.useGlobalPipes(new ValidationPipe());
   Settings.defaultZoneName = 'utc';
 
-  await app.listen(3000);
+  await app.listen(configService.get<number>('port'));
 }
+
 bootstrap();
